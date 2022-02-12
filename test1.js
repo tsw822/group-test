cd19c228ddba1f6fa62b85c2a07da57ee04209b4
seeutomorrow <larkingiuseppe@gmail.com>
2022-02-12 12:13:44 +0800

    git@github.com:seeutomorrow/test2022.git

---
+++ test1.js
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#define PI 3.14
+in("t main()
+{
+    float radius;
+    float high;
+    float volume;
+    scanf("%f", &radius);
+    scanf("%f", &hight);
+
+    volume = PI * radius * radius * high;
+
+    printf("%.2f", volume);
+    return 0;
+}
